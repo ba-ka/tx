7521f3343638f19b82ecb0dc913e70332c33a61b
Tholut A <tholutx@gmail.com>
2025-10-15 15:12:19 +0700

    feat: show current year on copyright text footer

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,7 @@ import { RouterLink, RouterView } from "vue-router";
     <RouterView />
 
     <footer>
-      <div class="footer-left">&copy; 2022 tx</div>
+      <div class="footer-left">&copy; {{ new Date().getFullYear() }} tx</div>
       <div class="footer-right">
         powered by
         <a href="https://ba-ka.org/" target="_blank">&#12400;-&#12363;</a>
