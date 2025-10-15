b1d5211a08477b127b5afe98ced1d7b6bdd1d9c3
Tholut A <tholutx@gmail.com>
2025-10-15 15:05:05 +0700

    feat: temp disable fetch api and replace with coming soon

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,16 @@
-<script setup>
+<!-- <script setup>
 import { RouterLink } from "vue-router";
 import { useFetch } from "../lib/fetch";
 const { data, error } = useFetch(
   "https://c.ba-ka.org/api/v1/kami?userid=61f3b1600c22a792c78228f1"
 );
-</script>
+</script> -->
 
 <template>
-  <div v-if="error">oh tidak, ada yang salah: {{ error.message }}</div>
+  coming soon!
+  <!-- <div v-if="error">oh tidak, ada yang salah: {{ error.message }}</div>
   <div v-else-if="data">
-    <div v-for="item in data.row">
+    <div v-for="item in data.row" v-bind:key="item.id">
       <RouterLink :to="{ name: 'x', params: { id: item.id } }"
         ><h3>
           {{ item.title }} <span>[{{ show_date(item.create_at) }}]</span>
@@ -17,7 +18,7 @@ const { data, error } = useFetch(
       >
     </div>
   </div>
-  <div v-else>memuat...</div>
+  <div v-else>memuat...</div> -->
 </template>
 
 <script>
